c53df500355b4fb7351c66bc4ffbf4c4e5592359
mbabchenko <mbabchenko@odl1905011.lohika.com>
2019-10-23 12:21:57 +0300

    Adds users

--- src/autocomplete.ts
+++ src/autocomplete.ts
@@ -1,1 +1,47 @@
 import "./assets/styles/styles.scss";
+import "./assets/styles/autocomplete.scss";
+import { fromEvent, Observable } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { switchMap, map, distinctUntilChanged, debounceTime, filter, pluck } from "rxjs/operators";
+import { IUser } from "./models/user.model";
+
+
+const autocompleteListEl = document.querySelector('.autocomplete__list');
+const autocompleteUrl = 'https://jsonplaceholder.typicode.com/users?username=';
+const addUserColumn = (name: string) => {
+    return `<li class="list-group-item">${name}</li>`
+};
+
+const fetchUserByName = (userName: string): Observable<IUser[]> =>
+  ajax.getJSON(`${autocompleteUrl}${userName}`);
+
+document.addEventListener('DOMContentLoaded', init)
+function init() {
+    const autocomplete: HTMLInputElement = document.querySelector("#autocomplete__input");
+
+    const source$ = fromEvent(autocomplete, 'input');
+
+    source$
+        .pipe(
+            map(e => (e.target as HTMLInputElement).value),
+            // TODO: add triming
+            // TODO: filter by min length of the 5
+            debounceTime(100),
+            distinctUntilChanged(),
+            switchMap(userName => fetchUserByName(userName))
+        )
+        .subscribe(renderAutocomplete)
+}
+
+
+function renderAutocomplete(users: IUser[]) {
+    console.log('--users', users);
+
+    const usersListHtml = users.map(item => {
+        return addUserColumn(item.username);
+    });
+
+    autocompleteListEl.innerHTML = usersListHtml.join();
+}
+
+
