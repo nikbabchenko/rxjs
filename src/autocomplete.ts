fa5954f8cdb86a865e46ad84925fe6a951905cb4
mbabchenko <mbabchenko@host-192-168-0-103.soho.net.ua>
2019-10-24 22:50:01 +0300

    organized imports

--- src/autocomplete.ts
+++ src/autocomplete.ts
@@ -1,14 +1,13 @@
-import "./assets/styles/styles.scss";
-import "./assets/styles/autocomplete.scss";
 import { fromEvent, Observable } from "rxjs";
 import { ajax } from "rxjs/ajax";
-import { switchMap, map, distinctUntilChanged, debounceTime, tap, pluck } from "rxjs/operators";
+import { debounceTime, distinctUntilChanged, map, switchMap, tap } from "rxjs/operators";
+import "./assets/styles/autocomplete.scss";
+import "./assets/styles/styles.scss";
 import { IUser } from "./models/user.model";
 
-
-const autocompleteListEl = document.querySelector('.autocomplete__list');
-const autocompleteUrl = 'https://jsonplaceholder.typicode.com/users?username=';
-const usersApiUrl = 'https://randomuser.me/api/?results=10';
+const autocompleteListEl = document.querySelector(".autocomplete__list");
+const autocompleteUrl = "https://jsonplaceholder.typicode.com/users?username=";
+const usersApiUrl = "https://randomuser.me/api/?results=10";
 
 // TODO: 1. Create a function that makes request to usersApiUrl using ajax.getJSON()
 // (the response will be in the following format https://randomuser.me/documentation#format) - `fetchUsers`
@@ -28,7 +27,6 @@ const usersApiUrl = 'https://randomuser.me/api/?results=10';
 // const clone = document.importNode(templateContent,  true);
 // document.querySelector(some-selector).appendChild(clone);
 
-
 // p.s. in response you can use pipeable operator `pluck` to take `results`
 // p.s. Typescript can't understand which Node is the image or anchor etc., so use such syntax to help TS
 // template = document.getElementById('user-card') as HTMLTemplateElement
@@ -38,41 +36,41 @@ const usersApiUrl = 'https://randomuser.me/api/?results=10';
 // TODO: finish aftercomplete's logic
 
 const addUserColumn = (name: string) => {
-    return `<li class="list-group-item">${name}</li>`
+  return `<li class="list-group-item">${name}</li>`;
 };
 
 const fetchUserByName = (userName: string): Observable<IUser[]> =>
   ajax.getJSON(`${autocompleteUrl}${userName}`);
 
-
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const autocomplete: HTMLInputElement = document.querySelector("#autocomplete__input");
-    const source$ = fromEvent(autocomplete, 'input');
+  const autocomplete: HTMLInputElement = document.querySelector(
+    "#autocomplete__input"
+  );
+  const source$ = fromEvent(autocomplete, "input");
 
-    // LOADER exists in Markup, just toggle class 'is-hidden'
-    source$
-        .pipe(
-            map(e => (e.target as HTMLInputElement).value),
-            // TODO: add triming
-            // TODO: filter by min length of the 4
-            // TODO: show Loader
-            debounceTime(100),
-            distinctUntilChanged(),
-            tap(userName => console.log('--next value', userName)),
-            switchMap(userName => fetchUserByName(userName)),
-            // TODO: add delay 300ms
-            // TODO: add finalize operator to hide the loader
-        )
-        .subscribe(renderAutocomplete)
+  // LOADER exists in Markup, just toggle class 'is-hidden'
+  source$
+    .pipe(
+      map(e => (e.target as HTMLInputElement).value),
+      // TODO: add triming
+      // TODO: filter by min length of the 4
+      // TODO: show Loader
+      debounceTime(100),
+      distinctUntilChanged(),
+      tap(userName => console.log("--next value", userName)),
+      switchMap(userName => fetchUserByName(userName))
+      // TODO: add delay 300ms
+      // TODO: add finalize operator to hide the loader
+    )
+    .subscribe(renderAutocomplete);
 }
 
-
 function renderAutocomplete(users: IUser[]) {
-    const usersListHtml = users.map(item => {
-        return addUserColumn(item.username);
-    });
+  const usersListHtml = users.map(item => {
+    return addUserColumn(item.username);
+  });
 
-    autocompleteListEl.innerHTML = usersListHtml.join();
+  autocompleteListEl.innerHTML = usersListHtml.join();
 }
